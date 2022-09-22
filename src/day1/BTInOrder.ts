53ee163dafe89e5d79d229505b1cd00becfd2847
J-Mad-08 <maddox.version1@gmail.com>
2022-09-21 22:33:09 -0400

    Adding BT files

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,15 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) return path;
+    // recurse
+    // pre
+    walk(curr.left, path);
+    // in order place
+    path.push(curr.value);
+    // post
+    walk(curr.right, path);
+    return path;
+}
 
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
