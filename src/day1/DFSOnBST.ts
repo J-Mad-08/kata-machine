1cd4ccf6eec748252c6a92ed65dafd8e04b2cd0f
J-Mad-08 <maddox.version1@gmail.com>
2022-09-24 12:12:04 -0400

    solved DFS with In Order Approach

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,7 +1,20 @@
+function walk(
+    curr: BinaryNode<number> | null,
+    needle: number,
+): boolean | undefined {
+    if (!curr) return;
+    if (curr.value === needle) return true;
 
+    let answer;
+    answer = walk(curr.left, needle);
+    // in order check
+    if (answer == true) return true;
+    answer = walk(curr.right, needle);
+
+    return answer == true;
+}
 
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-
-
-  return false;
+    let answer = walk(head, needle);
+    return answer == true;
 }
